060c4dc18028f707ccf4471e22ddbf17e107bcf4
Exevan <exevan110@gmail.com>
2014-09-13 00:33:47 +0200

    Basic logging and config fix

--- src/main/java/com/Exevan/Mod_01/Mod_01.java
+++ src/main/java/com/Exevan/Mod_01/Mod_01.java
@@ -2,17 +2,19 @@ package com.Exevan.Mod_01;
 
 import net.minecraftforge.common.config.Configuration;
 
-import com.Exevan.Mod_01.config.ConfigHandler;
+import com.Exevan.Mod_01.handlers.ConfigHandler;
 import com.Exevan.Mod_01.proxy.IProxy;
 import com.Exevan.Mod_01.reference.Reference;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid= Reference.MODID, name= Reference.MOD_NAME, version= Reference.VERSION)
+@Mod(modid= Reference.MODID, name= Reference.MOD_NAME, version= Reference.VERSION, guiFactory= Reference.GUI_FACTORY_CLASS)
 public class Mod_01 {
 	
 	@Mod.Instance(Reference.MODID)
@@ -24,16 +26,18 @@ public class Mod_01 {
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
 		ConfigHandler.init(e.getSuggestedConfigurationFile());
+		FMLCommonHandler.instance().bus().register(new ConfigHandler());
+		FMLLog.info("PreInit complete!");
 	}
 	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent e) {
-		
+		FMLLog.info("Init complete!");
 	}
 	
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent e) {
-		
+		FMLLog.info("PostInit complete!");
 	}
 
 }
---
+++ src/main/java/com/Exevan/Mod_01/client/gui/ModGuiConfig.java
@@ -0,0 +1,27 @@
+package com.Exevan.Mod_01.client.gui;
+
+import java.util.List;
+
+import com.Exevan.Mod_01.handlers.ConfigHandler;
+import com.Exevan.Mod_01.reference.Reference;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+import cpw.mods.fml.client.config.GuiConfig;
+import cpw.mods.fml.client.config.IConfigElement;
+
+public class ModGuiConfig extends GuiConfig{
+
+	public ModGuiConfig(GuiScreen guiScreen) {
+		
+		super(guiScreen, new ConfigElement(
+				ConfigHandler.config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(), 
+				Reference.MODID, 
+				false,
+				false, 
+				GuiConfig.getAbridgedConfigPath(ConfigHandler.config.toString()));
+		
+	}
+
+}
